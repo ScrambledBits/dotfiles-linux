90e5e78c6f7b2b614039312a6727b44ecfb1facc
Emilio Castro <1138771+ScrambledBits@users.noreply.github.com>
2022-11-17 23:31:44 +0000

    Changing install instructions and bashrc

--- install.sh
+++ install.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
 # Install homebrew
-sh homebrew-install.sh
+echo '' | bash homebrew-install.sh
 
 # Install ChezMoi
 
-sh chezmoi.sh -- init ScrambledBits/dotfiles-linux --apply --progress --verbose
+bash chezmoi.sh -- init ScrambledBits/dotfiles-linux --apply --progress --verbose
+
+# Install Oh-My-Bash
+
+bash oh-my-bash-install.sh
