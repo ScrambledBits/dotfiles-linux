0ba1d416ebc84657dddfc74b3ec0e9afc1b78241
Emilio Castro <1138771+ScrambledBits@users.noreply.github.com>
2022-11-17 22:20:16 +0000

    Fixing username check for codespaces

--- install.sh
+++ install.sh
@@ -5,4 +5,4 @@ sh homebrew-install.sh
 
 # Install ChezMoi
 
-sh chezmoi.sh -- init ScrambledBits/dotfiles-linux --apply --ssh --progress --verbose
+sh chezmoi.sh -- init ScrambledBits/dotfiles-linux --apply --progress --verbose
